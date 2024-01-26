54222b2b229178a85e79bac4c1e91f64dd4475b4
GabrieleLombard01 <Gabriellombardo2001@gmail.com>
2024-01-26 15:16:11 +0100

    Add project files.

---
+++ AsyncComm/Program.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMQ.Client;
+
+namespace AsyncComm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory 
+            { HostName = "localhost", Port=5672, 
+              UserName= "guest", Password="guest"
+            };
+            var connection = connectionFactory.CreateConnection();
+
+            connection.Close();
+        }
+    }
+}
